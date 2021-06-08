cc47ffe2f45c099c0fc9bcafdecd6668f3c780ae
Lee44 <jlee7772@yahoo.com>
2021-06-08 01:12:07 -0700

    Fixed Images. Modal too small

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -9,22 +9,11 @@ const ImageGrid = ({ setSelectedImg }) => {
     <div className="img-grid">
       {docs &&
         docs.map((doc) => (
-          <div
-            className="img-wrap"
-            key={doc.id}
-            layout
-            whileHover={{ opacity: 1 }}
-            s
+          <img
+            src={doc.url}
+            alt="uploaded pic"
             onClick={() => setSelectedImg(doc.url)}
-          >
-            <img
-              src={doc.url}
-              alt="uploaded pic"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 1 }}
-            />
-          </div>
+          />
         ))}
     </div>
   );
